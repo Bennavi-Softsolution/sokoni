aab5dd4a869e48ad0725a83f2fb9c7de865705d2
Levis Chisira <87423593+Proffesuer@users.noreply.github.com>
2024-07-25 17:03:50 +0300

    added a route

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/manage-product', function () {
+    return view('pages.manage-product');
+});
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
